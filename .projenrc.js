f773b52e99361bb1ee824cd650338a191cba1c6d
Janek <jhornung@oev.de>
2022-02-01 13:07:36 +0100

    chore: project created with projen

---
+++ .projenrc.js
@@ -0,0 +1,15 @@
+const { awscdk } = require('projen');
+const project = new awscdk.AwsCdkConstructLibrary({
+  author: 'Janek',
+  authorAddress: 'jhornung@oev.de',
+  cdkVersion: '2.1.0',
+  defaultReleaseBranch: 'main',
+  name: 'cdk-compliant-dynamodb',
+  repositoryUrl: 'https://github.com/jhornung/cdk-compliant-dynamodb.git',
+
+  // deps: [],                /* Runtime dependencies of this module. */
+  // description: undefined,  /* The description is just a string that helps people understand the purpose of the package. */
+  // devDeps: [],             /* Build dependencies for this module. */
+  // packageName: undefined,  /* The "name" in package.json. */
+});
+project.synth();
