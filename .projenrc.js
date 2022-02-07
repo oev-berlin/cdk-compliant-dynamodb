af6b164181dabd2f447a382ee39450fc4086c61b
Janek <jhornung@oev.de>
2022-02-07 14:37:40 +0100

    update project settings

--- .projenrc.js
+++ .projenrc.js
@@ -1,16 +1,13 @@
 const { awscdk } = require('projen');
 const project = new awscdk.AwsCdkConstructLibrary({
-  author: 'Janek',
-  authorAddress: 'jhornung@oev.de',
+  author: 'digitalcampus360 Berlin',
+  authorAddress: 'dc360@oev.de',
   cdkVersion: '2.1.0',
   defaultReleaseBranch: 'main',
   name: 'cdk-compliant-dynamodb',
-  repositoryUrl: 'https://github.com/jhornung/cdk-compliant-dynamodb.git',
+  repositoryUrl: 'https://github.com/oev-berlin/cdk-compliant-dynamodb.git',
 
-  // deps: [],                /* Runtime dependencies of this module. */
-  // description: undefined,  /* The description is just a string that helps people understand the purpose of the package. */
-  // devDeps: [],             /* Build dependencies for this module. */
-  // packageName: undefined,  /* The "name" in package.json. */
+  description: 'DynamoDb table that is compliant against most AWS Config rules',
 });
 const common_exclude = [
   'cdk.out',
